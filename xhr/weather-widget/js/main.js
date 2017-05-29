60c4450b7ec9df2159d37549cc02afd0af698b60
Fevralle <k.fomina@gmail.com>
2017-05-29 23:23:08 +0300

    Add files via upload

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,10 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', true);
 request.send();
-if (request.status === 200) {
-  const response = JSON.parse(request.responseText);
-  setData(response);
+request.addEventListener("load", onLoad)
+function onLoad() {
+  if (request.status === 200) {
+    const response = JSON.parse(request.responseText);
+    setData(response);
+  }
 }
